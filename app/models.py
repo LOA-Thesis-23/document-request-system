eec99028e42bad5ee9982985610873ff8309488f
JREM-Nameo <mjhunerey28@gmail.com>
2026-08-05 10:50:01 +0800

    Add staff database model and migration scripts

--- app/models.py
+++ app/models.py
@@ -4,7 +4,11 @@ from datetime import datetime, timezone
 
 @login_manager.user_loader
 def load_user(user_id):
-    return Student.query.get(int(user_id))
+    if user_id.startswith('staff_'):
+        return Staff.query.get(int(user_id.split('_')[1]))
+    if user_id.startswith('student_'):
+        return Student.query.get(int(user_id.split('_')[1]))
+    return None
 
 
 class Student(db.Model, UserMixin):
@@ -19,3 +23,19 @@ class Student(db.Model, UserMixin):
     status = db.Column(db.String(20), default='unverified')
     student_type = db.Column(db.String(20), nullable=True)
     date_created = db.Column(db.DateTime, default= lambda: datetime.now(timezone.utc))
+
+    def get_id(self):
+        return f'student_{self.id}'
+
+class Staff(db.Model, UserMixin):
+    __tablename__ = 'staff'
+
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    date_created = db.Column(db.DateTime, default= lambda: datetime.now(timezone.utc))
+
+    def get_id(self):
+        return f'staff_{self.id}'

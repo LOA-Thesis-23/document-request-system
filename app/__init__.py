70ffec08bb22e960849ea67d557cf82cc8fafa76
pafmarasigan@gmail.com <pafmarasigan@gmail.com>
2026-08-06 23:26:11 +0800

    added student redirect

--- app/__init__.py
+++ app/__init__.py
@@ -29,6 +29,8 @@ def create_app():
 
     from app import models
 
+    from app.routes.student import student_bp
+    app.register_blueprint(student_bp)
     from app.routes.auth import auth_bp
     app.register_blueprint(auth_bp)
 

cf565b624a80df066ebe4e2cf3c8f3242622ffda
pafmarasigan@gmail.com <pafmarasigan@gmail.com>
2026-08-07 00:58:18 +0800

    add request display to student dashboard

--- app/routes/student.py
+++ app/routes/student.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 student_bp = Blueprint('student', __name__, url_prefix='/student')
 
@@ -7,4 +7,9 @@ student_bp = Blueprint('student', __name__, url_prefix='/student')
 @student_bp.route('/home')
 @login_required
 def home():
-    return render_template('modals/studentside.html')
+    requests = sorted(
+        current_user.document_requests,
+        key=lambda r: r.date_requested,
+        reverse=True
+    )
+    return render_template('modals/studentside.html', requests=requests)

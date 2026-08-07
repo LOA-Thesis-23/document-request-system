450aa41370fa4a3dcecf242f53eca9fb0dd0468a
JREM-Nameo <mjhunerey28@gmail.com>
2026-08-07 12:40:55 +0800

    Add admin redirection after staff login and clean up admin routes

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,19 +1,13 @@
 from flask import Blueprint, render_template
-from flask_login import login_required,logout_user
-from flask import Blueprint, render_template, redirect, url_for, flash
-from app.models import Student
+from flask_login import login_required, current_user
+from app.decorators import admin_required
+
+
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 
 
 @admin_bp.route('/home')
 @login_required
+@admin_required
 def home():
     return render_template('modals/adminside.html')
-
-
-@admin_bp.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('You have been logged out.')
-    return redirect(url_for('index'))

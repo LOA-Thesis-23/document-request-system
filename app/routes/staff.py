2839b20a5c65618a95c989351341bc8b806469d3
JREM-Nameo <mjhunerey28@gmail.com>
2026-08-07 12:23:36 +0800

    Implement staff login functionality and role-based access control

--- app/routes/staff.py
+++ app/routes/staff.py
@@ -1,18 +1,13 @@
 from flask import Blueprint, render_template
-from flask_login import login_required,logout_user
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_required, current_user
+from app.decorators import staff_required
+
 
 staff_bp = Blueprint('staff', __name__, url_prefix='/staff')
 
 
 @staff_bp.route('/home')
 @login_required
+@staff_required
 def home():
-    return render_template('modals/staffside.html')
-
-@staff_bp.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('You have been logged out.')
-    return redirect(url_for('index'))
+    return render_template('modals/staffside.html', role=current_user.role)
